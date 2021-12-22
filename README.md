92acf94d7bda8c12c05f3f70a527dae2fcbfc7d6
lawless-m <matt-github@steponnopets.net>
2021-12-22 16:11:00 +0000

    iteration 1 almost worked

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ONSUD
+
+A package for turning the CSV files from GB's ONSUD open data into something more useful.
+
+The Office for Nation Statistics produces the ONS UPRN Directory (ONSUD) for Great Britain. 
+
+"The ONSUD relates the Unique Property Reference Number (UPRN) for each GB address from AddressBase® Epoch 88 to a range of current statutory administrative, electoral, health and other statistical geographies. The ONSUD is produced by ONS Geography, who provide geographic support to the Office for National Statistics (ONS) and geographic services used by other organisations. The ONSUD is issued every 6 weeks and is designed to complement the Ordnance Survey AddressBase® product."
+
+There doesn't seem to be a homepage for it, so this link will have to do
+
+https://geoportal.statistics.gov.uk/search?tags=onsud
+
--- src/ONSUD.jl
+++ src/ONSUD.jl
@@ -1,11 +1,96 @@
 module ONSUD
 
+using ZipFile
+using CSV
+using AODictionary
+using Serialization
+
 export check_for_update
+
+export generate
 # Write your package code here.
+
+const Grid = NamedTuple{(:e, :n), Tuple{Int64, Int64}}
+
+struct UPRNDB
+    grid::Dict{Int64, Grid}
+    field2uprn::AODict{Symbol, Dict{String, Vector{Int64}}}
+    uprn2dimension::Dict{Int64, UInt64}
+    dimensions::Dict
+    UPRNDB() = new(Dict{Int64, Grid}(), AODict{Symbol, Dict{String, Vector{Int64}}}(), Dict{Int64, UInt64}(), Dict())
+end
+
+function fill_fields!(db::UPRNDB, row)
+    for field in propertynames(row)
+        if ! (field in [:uprn, :gridgb1e, :gridgb1n])
+            db.field2uprn[field] = Dict{String, Vector{Int64}}()
+        end
+    end
+end
+
+function dimension(db::UPRNDB, row)
+    if length(db.field2uprn) == 0
+        fill_fields!(db, row)
+    end
+    map(k->getindex(row, k[2]), enumerate(keys(db.field2uprn)))
+end
+
+function dimension_index(db::UPRNDB, row)
+    dim = dimension(db, row)
+    hsh = hash(dim)
+    if ! (hsh in keys(db.dimensions))
+        db.dimensions[hsh] = dim
+    end
+    hsh
+end
+
+function add!(db::UPRNDB, row)
+    uprn = parse(Int, row.uprn)
+    db.grid[uprn] = (e=parse(Int64, row.gridgb1e), n=parse(Int64, row.gridgb1n))
+    db.uprn2dimension[uprn] = dimension_index(db, row)
+    for f in keys(db.field2uprn)
+        v = row[f]
+        if ismissing(v)
+            v = "missing"
+        end
+        if ! (v in keys(db.field2uprn[f]))
+            db.field2uprn[f][v] = [uprn]
+        else
+            push!(db.field2uprn[f][v], uprn)
+        end
+    end
+end
 
 function check_for_update()
     println("Visit https://geoportal.statistics.gov.uk/search?sort=-created&tags=onsud")
 end
 
+function row_readers(zipfile)
+    z = ZipFile.Reader(zipfile)
+    readers = []
+    for (i,file) in enumerate(z.files)
+        if startswith(file.name, "Data/") && endswith(file.name, ".csv")
+            push!(readers, (file.name, ()->CSV.Rows(read(z.files[i]))))
+        end
+    end
+    readers
+end
 
+function generate(zipfile="/home/matt/wren/UkGeoData/ONSUD_NOV_2021.zip", memofile="/home/matt/wren/UkGeoData/ONSUD_NOV_2021.sj")
+    readers = row_readers(zipfile)
+    db = UPRNDB()
+    for (fname, rows) in readers
+        println(fname)
+        for row in rows()
+            add!(db, row)
+        end
+    end    
+    
+    open(memofile, "w+") do io
+        serialize(io, db)
+    end
+    db
+end
+
+###
 end
