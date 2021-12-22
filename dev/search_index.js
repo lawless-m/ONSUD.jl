1b8e3472e6ea61ae276fc55a6ee81ab7d08d4fe3
Documenter.jl <documenter@juliadocs.github.io>
2021-12-22 11:53:57 +0000

    build based on 396d604

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ONSUD.jl","page":"ONSUD.jl","title":"ONSUD.jl","text":"","category":"section"},{"location":"","page":"ONSUD.jl","title":"ONSUD.jl","text":"Documentation for ONSUD.jl","category":"page"},{"location":"","page":"ONSUD.jl","title":"ONSUD.jl","text":"check_for_update","category":"page"}]
+}
