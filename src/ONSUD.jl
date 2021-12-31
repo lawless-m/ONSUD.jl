b4a9c6b45212c339e3ffd977a7ffdf3d89ca8314
lawless-m <matt-github@steponnopets.net>
2021-12-31 16:13:54 +0000

    no Index1024 aux

--- src/ONSUD.jl
+++ src/ONSUD.jl
@@ -16,9 +16,10 @@ const shift = 48
 
 tag(n, v) = UInt64(v) | (UInt64(n) << shift)
 tag(v::UInt64) = UInt32((UInt64(v) & mask) >> shift)
+key(n::Index1024.NodeInfo) = key(n.tagged_key)
+key(v::UInt) = UInt64(v) & ~mask
 
 const Grid = NamedTuple{(:e, :n), Tuple{Int64, Int64}}
-
 
 const DATADIR = "/home/matt/wren/UkGeoData/ONSUD_NOV_2021/Data"
 
@@ -80,32 +81,24 @@ function uprninfo(db::UPRNDB, uprn)
     info
 end
 
-function check_for_update()
-    println("Visit https://geoportal.statistics.gov.uk/search?sort=-created&tags=onsud")
-end
+check_for_update() = println("Visit https://geoportal.statistics.gov.uk/search?sort=-created&tags=onsud")
+    
+zipped_row_readers(zipfile) = map(file->(file.name, ()->CSV.Rows(read(file))), filter(f->startswith(f.name, "Data/") && endswith(f.name, ".csv"), ZipFile.Reader(zipfile).files))
+row_readers(datadir) = map(n->(n, ()->CSV.Rows(read(joinpath(datadir, n)))), readdir(datadir))
 
-function row_readers(zipfile)
-    z = ZipFile.Reader(zipfile)
-    readers = []
-    for (i,file) in enumerate(z.files)
-        if startswith(file.name, "Data/") && endswith(file.name, ".csv")
-            push!(readers, (file.name, ()->CSV.Rows(read(z.files[i]))))
-        end
-    end
-    readers
-end
+# @pipe ONSUD.row_readers(ONSUD.DATADIR) |> generate |> save(_, "/home/matt/wren/UkGeoData/uprndb.db")
 
-function generate(zipfile="/home/matt/wren/UkGeoData/ONSUD_NOV_2021.zip", memofile="/home/matt/wren/UkGeoData/ONSUD_NOV_2021.sj")
-    readers = row_readers(zipfile)
+function generate(readers)
     db = UPRNDB()
     for (fname, rows) in readers
         println(fname)
         for row in rows()
             add!(db, row)
         end
-    end    
-    save(db, memofile)
+    end
+    db
 end
+
 function save(db::UPRNDB, memofile)
     try
         open(memofile, "w+") do io
@@ -127,15 +120,14 @@ end
 
 ############### Index1024 stuff
 
-function index_csv!(io, n, lk, kvs, aux)
+function index_csv!(io, n, lk, kvs)
     readline(io) # header
     while ! eof(io)
         pos = position(io)
         uprn = parse(UInt64, readuntil(io, ","))
         lock(lk)
         try
-            kvs[uprn] = pos
-            aux[uprn] = n
+            kvs[uprn] = tag(n, pos)
         finally
             unlock(lk)
         end
@@ -145,20 +137,21 @@ end
 
 function index_datadir(datadir)
     kvs = Dict{UInt64, UInt64}()
-    aux = Dict{UInt64, UInt64}()
     lk = ReentrantLock()
     files = readdir(datadir)
     @sync for n in 1:length(files)
         Threads.@spawn open(joinpath(datadir, files[n]), "r") do io
-            index_csv!(io, n, lk, kvs, aux)
+            index_csv!(io, n, lk, kvs)
         end
     end
-    files, kvs, aux
+    files, kvs
 end
 
+# @time ONSUD.create_index1024(ONSUD.DATADIR, "/home/matt/wren/UkGeoData/onsud_nov_2021.index")
+
 function create_index1024(datadir, indexfile)
-    meta, kvs, aux = index_datadir(datadir)
-    build_index_file(indexfile, kvs; meta, aux)
+    meta, kvs = index_datadir(datadir)
+    build_index_file(indexfile, kvs; meta)
 end
 
 uprn_data(indexfile::AbstractString, datadir, uprn) = uprn_data(open_index(indexfile), datadir, uprn)
@@ -177,8 +170,8 @@ function uprn_data(idx::Index, datadir, uprn)
     if node === nothing
         return nothing
     end
-    open(joinpath(datadir, idx.meta[node[2]])) do io 
-        csv(io, node[1])
+    open(joinpath(datadir, idx.meta[tag(node)])) do io 
+        csv(io, key(node))
     end
 end
 
