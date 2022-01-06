aa5b2610524b24a682dcec73814f7cff9d7a61cc
lawless-m <matt-github@steponnopets.net>
2022-01-06 16:48:02 +0000

    postcode database now working inc. pcode_info

--- src/ONSUD.jl
+++ src/ONSUD.jl
@@ -11,11 +11,13 @@ export check_for_update
 
 export generate
 
-export uprn_data, create_index1024
-
+export uprn_data, create_index1024, index_by_postcode, open_pcodedb, pcode_info
 
 const mask = 0xffff000000000000 # 64k files should be enough for anybody
 const shift = 48
+
+const ENDim = typeof((e=zero(UInt64), n=zero(UInt64), dim=zero(UInt64)))
+const EN = typeof((e=zero(Int64), n=zero(Int64)))
 
 tag(n, v) = UInt64(v) | (UInt64(n) << shift)
 tag(v::UInt64) = UInt32((UInt64(v) & mask) >> shift)
@@ -24,14 +26,15 @@ key(v::UInt) = UInt64(v) & ~mask
 
 const Grid = NamedTuple{(:e, :n, :pc), Tuple{UInt64, UInt64, UInt64}}
 
-const DATADIR = "/home/matt/wren/UkGeoData/ONSUD_NOV_2021/Data"
+const GEODIR = "/home/matt/wren/UkGeoData"
+const DATADIR = joinpath(GEODIR, "ONSUD_NOV_2021/Data")
 
 struct UPRNDB
     grid::Dict{Int64, Grid}
-    field2uprn::AODict{Symbol, Dict{String, Vector{Int64}}}
+    field2uprn::Dict{Symbol, Dict{String, Vector{Int64}}}
     uprn2dimension::Dict{Int64, UInt64}
     dimensions::Dict
-    UPRNDB() = new(Dict{Int64, Grid}(), AODict{Symbol, Dict{String, Vector{Int64}}}(), Dict{Int64, UInt64}(), Dict())
+    UPRNDB() = new(Dict{Int64, Grid}(), Dict{Symbol, Dict{String, Vector{Int64}}}(), Dict{Int64, UInt64}(), Dict())
 end
 
 function fill_fields!(db::UPRNDB, row)
@@ -49,7 +52,11 @@ function dimension(db::UPRNDB, row)
         fill_fields!(db, row)
     end
 
-    map(k->str(getindex(row, k[2])), enumerate(keys(db.field2uprn)))
+    dim = Dict{Symbol, String}()
+    for k in keys(db.field2uprn)
+        dim[Symbol(k)] = str(row[k])
+    end
+    (;dim...)
 end
 
 function dimension_index(db::UPRNDB, row)
@@ -87,31 +94,45 @@ check_for_update() = println("Visit https://geoportal.statistics.gov.uk/search?s
 zipped_row_readers(zipfile) = map(file->(file.name, ()->CSV.Rows(read(file))), filter(f->startswith(f.name, "Data/") && endswith(f.name, ".csv"), ZipFile.Reader(zipfile).files))
 row_readers(datadir) = map(n->(n, ()->CSV.Rows(read(joinpath(datadir, n)))), readdir(datadir))
 
-# @pipe ONSUD.row_readers(ONSUD.DATADIR) |> generate |> save(_, "/home/matt/wren/UkGeoData/uprndb.db")
+# @pipe ONSUD.row_readers(ONSUD.DATADIR) |> generate |> save(joinpath(ONSUD.GEODIR, "uprndb.db"), _)
+
+function generate(reader::Tuple)
+    println(reader[1])
+    db = UPRNDB()
+    foreach(row->add!(db, row), reader[2]())
+    db 
+end
 
 function generate(readers)
-    db = UPRNDB()
-    for (fname, rows) in readers
-        println(fname)
-        foreach(row->add!(db, row), rows())
+    dbs = Vector{UPRNDB}(undef, length(readers))
+    Threads.@threads for i in 1:length(readers)
+        dbs[i] = generate(readers[i])
     end
-    db
+    uprndb = UPRNDB()
+    for db in dbs
+        merge!(uprndb.grid, db.grid)
+        merge!(uprndb.field2uprn, db.field2uprn)
+        merge!(uprndb.uprn2dimension, db.uprn2dimension)
+        merge!(uprndb.dimensions, db.dimensions)
+    end
+    uprndb
 end
 
-function save(db::UPRNDB, io::IO)
-    serialize(io, db)
-    db
+
+function save(io::IO, object)
+    serialize(io, object)
+    object
 end
 
-function save(db::UPRNDB, memofile)
+function save(memofile::AbstractString, object)
     try
         open(memofile, "w+") do io
-            save(db, io)
+           return save(io, object)
         end
     catch e
         println(stderr, e)
     end
-    db
+    object
 end
 
 function load(memofile)
@@ -125,7 +146,111 @@ end
 ############### Index1024 stuff
 
 function postcode_to_UInt64(pc) 
+    if ismissing(pc)
+        return 0
+    end
     reduce((a,c) -> UInt64(a) << 8 + UInt8(c), filter(c->c != ' ', collect(pc)), init=0)
+end
+
+function pc_index(db::UPRNDB)
+    pcds = Dict{UInt64, Set{ENDim}}()
+    for uprn in keys(db.grid)
+        (;e,n,pc) =  db.grid[uprn]
+        dim = db.uprn2dimension[uprn]
+        if ! (pc in keys(pcds))
+            pcds[pc] = Set{ENDim}([(;e,n,dim)])
+        else
+            push!(pcds[pc], (;e,n,dim))
+        end
+    end
+    pcds
+end
+
+Base.write(io::IO, endim::ENDim) = write(io, endim.e) + write(io, endim.n) + write(io, endim.dim)
+Base.read(io::IO, ::Type{ENDim}) = (e=read(io, UInt64), n=read(io, UInt64), dim=read(io, UInt64))
+Base.write(io::IO, endims::Set{ENDim}) = reduce((a,endim)->a+write(io, endim), endims, init=write(io, UInt64(length(endims))))
+Base.read(io::IO, ::Type{Set{ENDim}}) = Set{ENDim}([read(io, ENDim) for i in 1:read(io, UInt64)])
+
+function write_and_index_pcode_data(io::IO, pcds)
+    kvs = Dict{UInt64, Index1024.DataAux}()
+    for pc in keys(pcds)
+        kvs[pc] = (data=position(io), aux=length(pcds[pc])) # may as well put something in aux
+        write(io, pcds[pc])
+    end
+    kvs
+end
+
+#==
+    index_by_postcode("nov_2021.uprndb")
+    pdb = ONSUD.open_pcodedb(joinpath(ONSUD.GEODIR, "pcode.db.index"))
+    pcode_info(pdb, "S17 3BB")
+    
+    index_by_postcode("test.uprndb"; pcindexfile="pctest.db.index", datadir="ONSUD_NOV_2021/Test")
+    pdb = ONSUD.open_pcodedb(joinpath(ONSUD.GEODIR, "pctest.db.index"))
+    pcode_info(pdb, "S17 3BB")
+
+    
+    index_by_postcode("bbtest.uprndb"; pcindexfile="bbtest.db.index", datadir="ONSUD_NOV_2021/BB")
+    pdb = ONSUD.open_pcodedb(joinpath(ONSUD.GEODIR, "bbtest.db.index"))
+    pcode_info(pdb, "S17 3BB")
+==#
+
+function index_by_postcode(uprndbfile::AbstractString; pcindexfile="pcode.db.index", geodir="", datadir="")
+    if geodir == ""
+        geodir = ONSUD.GEODIR
+    end
+    if datadir == ""
+        datadir = ONSUD.DATADIR
+    end
+    uprndb = load(joinpath(geodir, uprndbfile))
+    if uprndb === nothing
+        uprndb = save(joinpath(geodir, uprndbfile), generate(row_readers(joinpath(geodir, datadir))))
+    end
+    index_by_postcode(uprndb; pcindexfile, geodir)
+end
+
+function index_by_postcode(uprndb::UPRNDB; pcindexfile="pcode.db.index", geodir="")
+    if geodir == ""
+        geodir = ONSUD.GEODIR
+    end
+    open(joinpath(geodir, pcindexfile), "w+") do io
+        write(io, zero(Int64)) # placeholder for index_pos
+        write(io, zero(Int64)) # placeholder for dimension_pos
+        dimension_pos = position(io)    
+        @assert dimension_pos > 0
+        save(io, uprndb.dimensions)
+        Index1024.nextblock(io)    
+        kvs = write_and_index_pcode_data(io, pc_index(uprndb)) # pcode=>(data=offset, aux=dimcount)
+        Index1024.nextblock(io)
+        index_pos = position(io)    
+        @assert index_pos > 0
+        build_index_file(io, kvs; meta=String["Self Contained Indexed Database"])
+        seekstart(io)
+        write(io, index_pos)
+        write(io, dimension_pos)
+    end
+end
+
+function open_pcodedb(pcodefn)
+    io = open(pcodefn, "r")
+    index_pos = read(io, Int64)
+    dimension_pos = read(io, Int64)
+    seek(io, dimension_pos)
+    dimensions = deserialize(io)
+    seek(io, index_pos)
+    index = open_index(io)
+    (;index, dimensions)
+end
+
+pcode_info(pcodefn::AbstractString, pcode) = pcode_info(open_pcodedb(pcodefn), pcode)
+
+function pcode_info(pcodedb::NamedTuple, pcode)
+    node = search(pcodedb.index, postcode_to_UInt64(pcode))
+    if node === nothing
+        return nothing
+    end
+    seek(pcodedb.index.io, node.data)
+    read(pcodedb.index.io, Set{ENDim})
 end
 
 function by_uprn!(io, n, lk, kvs)
@@ -144,7 +269,7 @@ function by_uprn!(io, n, lk, kvs)
 end
 
 function index_datadir(datadir, index_by!)
-    kvs = Dict{UInt64, typeof((data=UInt64(0), aux=UInt64(0)))}()
+    kvs = Dict{UInt64, Index1024.DataAux}()
     lk = ReentrantLock()
     files = readdir(datadir)
     @sync for n in 1:length(files)
@@ -156,7 +281,6 @@ function index_datadir(datadir, index_by!)
 end
 
 #==
-
 @time ONSUD.create_index1024(ONSUD.DATADIR, "/home/matt/wren/UkGeoData/onsud_nov_2021.uprn.index", by_uprn!)
 using BenchmarkTools
 @benchmark ONSUD.uprn_data(ONSUD.DATADIR, "/home/matt/wren/UkGeoData/onsud_nov_2021.uprn.index", 10015278860)
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,8 @@ function buff(size=3000)
     io
 end
 
-function testdata(testdir)
+function testdata(;geodir, datadir)
+    testdir = joinpath(geodir, datadir)
     if !isdir(testdir)
         println(stderr, "TestData not present, so skipping")
         return true
@@ -18,16 +19,47 @@ function testdata(testdir)
     uprn_data(testdir, "/tmp/onsud_test.index", 10013555700) !== nothing
 end
 
-function test_generate(testdir)
+function test_generate(uprndbfile; geodir, datadir)
+    testdir = joinpath(geodir, datadir)
     if !isdir(testdir)
         println(stderr, "TestData not present, so skipping")
         return true
     end
-    ONSUD.save(generate(ONSUD.row_readers(testdir)), buff())
+    open(joinpath(geodir, uprndbfile), "w+") do io 
+        ONSUD.save(io, generate(ONSUD.row_readers(testdir)))
+    end
+    filesize(joinpath(geodir, uprndbfile)) > 0
 end
+
+function test_pcodedb(uprndbfile; pcindexfile, geodir, datadir)
+    testdir = joinpath(geodir, datadir)
+    if !isdir(testdir)
+        println(stderr, "TestData not present, so skipping")
+        return true
+    end
+    index_by_postcode(uprndbfile; pcindexfile, geodir, datadir)
+    filesize(joinpath(geodir, pcindexfile)) > 0
+end
+
+function test_openpcodedb(pcodefn)
+    if ! isfile(pcodefn)
+        println(stderr, "Test pcodedb not present, so skipping")
+        return true
+    end
+
+    index, dimensions =  open_pcodedb(pcodefn)
+    isa(index, ONSUD.Index1024.Index) && isa(dimensions, Dict)
+end
+
+const geodir = "/home/matt/wren/UkGeoData"
+const datadir = "ONSUD_NOV_2021/Test"
+const uprndbfile = "test.uprndb"
+const pcindexfile = "pcode.testdb.index"
 
 @testset "ONSUD.jl" begin
     @test isa(ONSUD.UPRNDB(), ONSUD.UPRNDB)
-    @test isa(test_generate("/home/matt/wren/UkGeoData/ONSUD_NOV_2021/Test"), ONSUD.UPRNDB)
-    @test testdata("/home/matt/wren/UkGeoData/ONSUD_NOV_2021/Test")
+    @test testdata(;datadir, geodir)
+    @test test_generate(uprndbfile; geodir, datadir)
+    @test test_pcodedb(uprndbfile; pcindexfile, geodir, datadir)
+    @test test_openpcodedb(joinpath(geodir, pcindexfile))
 end
